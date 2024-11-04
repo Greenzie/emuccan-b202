ae0ad106b5dfd7d56f966958d739cd61534b8e6d
Esteban Martinena <orensbruli@gmail.com>
2024-11-04 08:55:25 +0100

    emuccan service start in postinstall

--- driver/post_install.sh
+++ driver/post_install.sh
@@ -24,4 +24,10 @@ echo "${MODULE_NAME}" | sudo tee /etc/modules-load.d/${MODULE_NAME}.conf
 
 depmod -a
 
+# Reload udev rules (from postinst)
+udevadm control --reload-rules && udevadm trigger
+
+# enable emuccan.service
+systemctl enable emuccan
+
 modprobe ${MODULE_NAME} || true
